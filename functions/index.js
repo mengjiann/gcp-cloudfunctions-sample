364e459791dc9728486b92a894c6b859156c2f27
leemj <leemj@sph.com.sg>
2019-12-24 12:11:49 +0800

    Updated code to test on changes.

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,4 @@
 exports.helloWorld = (req, res) => {
-  let message = req.query.message || req.body.message || 'Hello World, MJ!';
+  let message = req.query.message || req.body.message || 'Hello World Again, MJ !';
   res.status(200).send(message);
 };
